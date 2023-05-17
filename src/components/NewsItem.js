600034ccb0660665ec48f2cf3eaa537208e295d3
Pooja Kumari <86198133+Poojakpph@users.noreply.github.com>
2023-05-18 01:50:05 +0530

    Update NewsItem.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,16 +3,16 @@ import React, { Component } from 'react'
 export  class NewsItem extends Component {
    
   render() {
-    let {title, description, imgurl}= this.props;
+    let {title, description, imgurl, newsUrl}= this.props;
 
     return (
       <div className="my-3">
          <div  className="card" style={{width: "18rem"}}>
-            <img src={imgurl}  className="card-img-top" alt="..."/>
+            <img src={!imgurl?"https://c.ndtvimg.com/2023-04/e9pqohd_tim-david-bcci_625x300_22_April_23.jpg? im=FaceCrop,algorithm=dnn,width=1200,height=675":imgurl}  className="card-img-top" alt="..."/>
             <div  className="card-body">
             <h5  className="card-title">{title}</h5>
             <p  className="card-text">{description}</p>
-            <a href="/newsdetail/"  className="btn btn-sm btn-primary">Read More</a>
+            <a href={newsUrl}  className="btn btn-sm btn-dark">Read More</a>
         </div>
        </div>
     </div>
