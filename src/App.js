3418852156f0edfdcd701744961becd886a2f1ee
Pooja Kumari <86198133+Poojakpph@users.noreply.github.com>
2023-05-19 01:51:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import './App.css';
-
 import React, { Component } from 'react'
 
-import News from './components/News';
-import Navbar from './components/Navbar';
-
-
-export default class App extends Component {
+export  class NewsItem extends Component {  
   render() {
+    let {title, description, imgurl, newsUrl}= this.props;
+
     return (
-      <div>
-       <Navbar/>
-       <News/>
-      </div>
+      <div className="my-3">
+         <div  className="card">
+            <img src={!imgurl?"https://c.ndtvimg.com/2023-04/e9pqohd_tim-david-bcci_625x300_22_April_23.jpg? im=FaceCrop,algorithm=dnn,width=1200,height=675":imgurl}  className="card-img-top" alt="..."/>
+            <div  className="card-body">
+            <h5  className="card-title">{title}</h5>
+            <p  className="card-text">{description}</p>
+            <a href={newsUrl}  className="btn btn-sm btn-dark">Read More</a>
+        </div>
+       </div>
+    </div>
     )
   }
 }
+export default NewsItem
 
