260199b550d52c00e4b644bf7190689d3cfb0623
Pooja Kumari <86198133+Poojakpph@users.noreply.github.com>
2023-05-21 13:02:51 +0530

    Update NewsItem.js

--- Optimized Version/NewsItem.js
+++ Optimized Version/NewsItem.js
@@ -8,7 +8,16 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card">
-          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}>{source} </span>
+          <div style=
+            {{
+              display:'flex',
+              justifyContent: 'flex-end',
+              position: 'absolute',
+              right: '0'
+            }} >
+         
+           <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}>{source} 
+           </span> </div>
           <img src={!imgurl ? "https://c.ndtvimg.com/2023-04/e9pqohd_tim-david-bcci_625x300_22_April_23.jpg? im=FaceCrop,algorithm=dnn,width=1200,height=675" : imgurl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title} 
